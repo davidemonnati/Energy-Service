57bba5a4de43f0e492907bbd003696d6495b90fd
Davide Monnati <davide.monnati@gmail.com>
2019-12-03 22:55:53 +0100

    fixed compatibility with ESP32

--- backend/build/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
--- backend/build/dto/apartments.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ApartmentDTO = /** @class */ (function () {
-    function ApartmentDTO() {
-    }
-    return ApartmentDTO;
-}());
-exports.ApartmentDTO = ApartmentDTO;
