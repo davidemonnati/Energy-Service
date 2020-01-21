83192c6c683687ae6cc55ee437d9c7d184578ff3
Davide Monnati <davide.monnati@gmail.com>
2020-01-21 15:50:19 +0100

    clean code

--- backend/app/dto/consumption.dto.ts
+++ backend/app/dto/consumption.dto.ts
@@ -3,7 +3,6 @@ import { Apartment } from '../entities/apartment.entity';
 export class ConsumptionDTO {
     readonly id!: number;
     readonly datetime !: string;
-    // readonly time !: string;
     readonly value !: number;
     readonly apartment!: Apartment;
 }
--- backend/app/entities/consumption.entity.ts
+++ backend/app/entities/consumption.entity.ts
@@ -1,4 +1,4 @@
-import {Entity, ManyToOne, PrimaryColumn, Column, PrimaryGeneratedColumn } from "typeorm";
+import {Entity, ManyToOne, Column, PrimaryGeneratedColumn } from "typeorm";
 import {Apartment} from "./apartment.entity";
 
 @Entity("Consumption")
@@ -6,7 +6,7 @@ export class Consumption {
     @PrimaryGeneratedColumn()
     public id: number;
 
-    @Column({type: 'timestamp'})
+    @Column({type: 'varchar', length: 150})
     public datetime: string;
 
     // @Column({type: 'varchar', length: 150})
@@ -18,10 +18,9 @@ export class Consumption {
     @ManyToOne(type => Apartment, apartments => apartments.consumptions, {nullable: false, onDelete: "CASCADE"})
     public apartment: Apartment;
 
-    constructor(id: number, datetime: string, time: string, value: number, apartment: Apartment){
+    constructor(id: number, datetime: string, value: number, apartment: Apartment){
         this.id = id;
         this.datetime = datetime;
-        // this.time = time;
         this.value = value;
         this.apartment = apartment;
     }
