23e7ce8948fa11ae24965e95f4e6a6ec97fcc23d
Riccardo Fioretti <riccardofio97@gmail.com>
2020-04-02 00:14:45 +0200

    Add dates range filter

--- backend/app/services/apartment.service.ts
+++ backend/app/services/apartment.service.ts
@@ -71,6 +71,40 @@ export class ApartmentService {
         return await consumptions;
     }
 
+    async getConsumptionsByRangeDates(row: string, number: string, day: string, mounth: string, year: string, day2: string, mounth2: string, year2: string): Promise<Consumption[]> {
+        var getDates = function(startDate: Date, endDate: Date) { 
+            let dates:Date[] = new Array();
+            let currentDate: Date = startDate;
+            while (currentDate <= endDate) { 
+                dates.push(currentDate);
+                let date = new Date(currentDate);
+                date.setDate(date.getDate() + 1);
+                currentDate = date;
+            }
+            return dates;
+        }
+        
+        let apartment = await this.apartmentRepository.getApartment(row, number);
+        let consumptions:Consumption[] = new Array();
+        let datetime = new Date(year+"-"+mounth+"-"+day);
+        let datetime2 = new Date(year2+"-"+mounth2+"-"+day2);
+        var dates:Date[] = getDates(datetime,datetime2);
+
+        for(let i=0; i<dates.length;i++) {
+            var dateString = dates[i].getFullYear() + "/" + (dates[i].getMonth()+1) + "/" + dates[i].getDate();;
+            for(let consumption of apartment.consumptions)
+            {
+                const consumptionsOfApartment = await this.consumptionRepository.getOneByid(consumption.id);
+                if(consumptionsOfApartment.datetime.includes(dateString)){
+                    consumptions.push(consumptionsOfApartment);
+                }
+            }
+        }
+        consumptions.sort((a,b) => b.datetime > a.datetime ? -1 : 1);
+
+        return await consumptions;
+    }
+
     async getAll(): Promise<Apartment[]> {
         return await this.apartmentRepository.getAll();
     }
