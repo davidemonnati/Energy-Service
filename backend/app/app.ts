bc5547caaf3e7800c39c1b4640a89cf28b2f1e71
Davide Monnati <davide.monnati@gmail.com>
2020-04-07 12:25:19 +0200

    Auth

--- backend/app/app.ts
+++ backend/app/app.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import { useContainer as typeormUseContainer, createConnection } from 'typeorm';
 import { Container } from 'typedi';
-import { createExpressServer, useContainer as routingUseContainer } from 'routing-controllers';
+import { createExpressServer, useContainer as routingUseContainer, Action } from 'routing-controllers';
 import { ApartmentController } from './controllers/apartment.controller';
 import { ConsumptionController } from './controllers/consumption.controller';
+import { isUndefined } from 'util';
+import { initializeApp, credential, auth } from 'firebase-admin';
 
 typeormUseContainer(Container)
 routingUseContainer(Container)
@@ -16,11 +18,37 @@ const app = createExpressServer({
         ApartmentController,
         ConsumptionController,
     ],
+    authorizationChecker: async(action: Action) => {
+        try{
+            var idToken = action.request.headers['authorization'].split(' ')[1];
+            const decodedToken = await auth().verifyIdToken(idToken)
+                .then(function(decodedToken){
+                    return decodedToken.uid;
+                })
+                .catch(function(err){
+                    console.log(err);
+                })
+
+                if(isUndefined(decodedToken))
+                    return false;
+                
+                return true;
+        }
+        catch{
+            return false;
+        }
+    },
     classTransformer: true,
     validation: true
 });
 
 app.listen(port, () => {
+    var serviceAccount = require('../ServiceAccountKey.json');
+    initializeApp({
+        credential: credential.cert(serviceAccount),
+        databaseURL: 'https://energyservice-679ff.firebaseio.com'
+    });
+    
     console.log("Service listening on port " + port);
 })
 
