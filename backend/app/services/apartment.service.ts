76e3b9f7b79ccc2e9d9e3a893a5cfed0f3dead22
Riccardo Fioretti <riccardofio97@gmail.com>
2020-05-15 01:22:07 +0200

    Refactor and fix

--- backend/app/services/apartment.service.ts
+++ backend/app/services/apartment.service.ts
@@ -5,6 +5,7 @@ import { Apartment } from '../entities/apartment.entity';
 import { ApartmentDTO } from '../dto/apartments.dto';
 import { Consumption } from '../entities/consumption.entity';
 import { ConsumptionService } from './consumption.service';
+import { getRepository } from 'typeorm';
 
 Service()
 export class ApartmentService {
@@ -44,27 +45,31 @@ export class ApartmentService {
 
     async getConsumptionsOfApartmentName(row: string, number: string): Promise<Consumption[]> {
         let apartment = await this.apartmentRepository.getApartment(row, number);
-        let consumptions:Consumption[] = new Array();
+        /*let consumptions:Consumption[] = new Array();
         for(let consumption of apartment.consumptions)
         {
             const consumptionsOfApartment = await this.consumptionRepository.getOneByid(consumption.id);
             consumptions.push(consumptionsOfApartment);
         }
-        consumptions.sort((a,b) => this.toTimestamp(b.datetime) > this.toTimestamp(a.datetime) ? -1 : 1);
+        //consumptions.sort((a,b) => this.toTimestamp(b.datetime) > this.toTimestamp(a.datetime) ? -1 : 1);*/
 
-        return await consumptions;
+        return await apartment.consumptions;
     }
 
     async getConsumptionsByDate(row: string, number: string, year: string, mounth: string, day: string): Promise<Consumption[]> {
         let apartment = await this.apartmentRepository.getApartment(row, number);
         let consumptions:Consumption[] = new Array();
         let datetime = this.getDateString(day, mounth, year);
-        for(let consumption of apartment.consumptions) {
-            const consumptionsOfApartment = await this.consumptionRepository.getOneByid(consumption.id);
-            if(consumptionsOfApartment.datetime.includes(datetime)){
-                consumptions.push(consumptionsOfApartment);
+        /*for(let consumption of apartment.consumptions) {
+            //const consumptionsOfApartment = await this.consumptionRepository.getOneByid(consumption.id);
+            if(consumption.datetime.includes(datetime)){
+                consumptions.push(consumption);
             }
-        }
+        }*/
+        consumptions = await getRepository(Consumption)
+                .createQueryBuilder("consumption")
+                .where("consumption.datetime like :datetimes", {datetimes: datetime + '%'})
+                .getMany();
         consumptions.sort((a,b) => this.toTimestamp(b.datetime) > this.toTimestamp(a.datetime) ? -1 : 1);
 
         return await consumptions;
@@ -78,17 +83,28 @@ export class ApartmentService {
         var dates: Date[] = this.getDateRange(datetime,datetime2);
 
         for(let i=0; i<dates.length;i++) {
+            
             var dateString = this.getDateString(dates[i].getDate().toString(), (dates[i].getMonth() + 1).toString(),
+                dates[i].getFullYear().toString());
+                console.log(dateString);
+            
+            consumptions = consumptions.concat(await getRepository(Consumption)
+            .createQueryBuilder("consumption")
+            .where("consumption.datetime like :datetimes", {datetimes: dateString + '%'})
+            .getMany());
+            /*var dateString = this.getDateString(dates[i].getDate().toString(), (dates[i].getMonth() + 1).toString(),
                 dates[i].getFullYear().toString());
 
             for(let consumption of apartment.consumptions){
-                const consumptionsOfApartment = await this.consumptionRepository.getOneByid(consumption.id);
-                if(consumptionsOfApartment.datetime.includes(dateString)){
-                    consumptions.push(consumptionsOfApartment);
+                //const consumptionsOfApartment = await this.consumptionRepository.getOneByid(consumption.id);
+                if(consumption.datetime.includes(dateString)){
+                    consumptions.push(consumption);
                 }
-            }
+            }*/
         }
         consumptions.sort((a,b) => this.toTimestamp(b.datetime) > this.toTimestamp(a.datetime) ? -1 : 1);
+
+        
 
         return await consumptions;
     }
@@ -106,7 +122,7 @@ export class ApartmentService {
         day = ('0' + day).slice(-2)
         mounth = ('0' + mounth).slice(-2);
         
-        return day + '/' + mounth + '/' + year;
+        return year + '-' + mounth + '-' + day;
     }
 
     private getDateRange(startDate: Date, endDate: Date): Date[] {
